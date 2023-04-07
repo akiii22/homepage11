984808cfc32d61583b648392be0c6d3b16ecd254
jeromee <119032315+akiii22@users.noreply.github.com>
2023-04-07 22:11:14 +0800

    Add files via upload

---
+++ Jclass/script.js
@@ -0,0 +1,37 @@
+let menuBtn = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.header .flex .navbar');
+
+
+menuBtn.onclick = () =>{
+    menuBtn.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+
+var swiper = new Swiper(".service-slider", {
+      spaceBetween: 20,
+      grabCursor: true,
+      loop: true,
+     
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+        640: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3  ,
+        },
+      },
+    
+});
+
+
+
+
